4ccf4904669ebf1b0707438bc404bf0495080dda
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-04-18 23:46:31 +0200

    build\git-changelog.php. New code.

--- build/git-changelog.php
+++ build/git-changelog.php
@@ -11,7 +11,21 @@ require __DIR__ . '/../_composer/vendor/autoload.php';
 
 // Instantiate the library's renderer.
 $changelog = new MarkDown('../');
+$changelog->commitUrl = 'https://github.com/GHSVS-de/plg_content_prismhighlighterghsvs/commit/{hash}';
+
+$changelog->setOptions([
+	#'logHeader'           => 'Changelog',
+	#'headTagName'         => 'Upcoming changes',
+	#'headTagDate'         => 'Undetermined',
+	#'noChangesMessage'    => 'No changes.',
+	#'addHashes'           => true,
+	#'includeMergeCommits' => false,
+	#'tagOrderBy'          => 'creatordate',
+	#'tagOrderDesc'        => true,
+	// GHSVS. No title ordering! DESC | ASC.
+	'titleOrder'          => '',
+]);
 
 // Build and save the changelog with all defaults.
 $changelog->build();
-$changelog->save('../CHANGELOG.md');
+$changelog->save(__DIR__ . '/../dist/CHANGELOG.md');
